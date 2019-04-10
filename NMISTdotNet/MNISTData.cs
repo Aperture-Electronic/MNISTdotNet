260476e9ba4990f93bd74738508cc16c00d1cf0d
小野寺つさき <ApertureElectronic@outlook.com>
2019-04-10 12:52:00 +0800

    Changes

---
+++ NMISTdotNet/MNISTData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NMISTdotNet
+{
+    class MNISTData
+    {
+        [LoadColumn(0)]
+        public byte[] ImageVector;
+    }
+
+    class MNISNumber
+    {
+        [ColumnName("Number")]
+        public int Number;
+    }
+}
--- NMISTdotNet/Program.cs
+++ NMISTdotNet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Data.DataView;
 using Microsoft.ML;
 
 namespace NMISTdotNet
@@ -7,7 +8,14 @@ namespace NMISTdotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Using a 0 seed to initialize the machine learning context
+            MLContext context = new MLContext(seed: 0);
+
+            // Create a dataview for training
+            IDataView trainingDataView = context.Data.LoadFromTextFile<MNISTData>("mnist_training.dat");
+
+            
+
         }
     }
 }
