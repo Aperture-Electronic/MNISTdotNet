653c37dad30ec52c27735d401fd18b80669dd180
小野寺つさき <ApertureElectronic@outlook.com>
2019-04-10 12:27:04 +0800

    添加项目文件。

---
+++ NMISTdotNet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.ML;
+
+namespace NMISTdotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
